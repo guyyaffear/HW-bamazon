817d4b1c1dac47419a32a54d4e161242b51c5b9e
GuyYaffeErmoza <guyyaffear@gmail.com>
2020-08-07 00:16:09 +0300

    adding seed file

--- bamazonManager.js
+++ bamazonManager.js
@@ -7,7 +7,7 @@ var connection = mysql.createConnection({
     // Your username
     user: "root",
     // Your password
-    password: "123456",
+    password: "",
     database: "bamazon"
 });
 // console.log(connection);
